fabb514a55073e674fa1aed445a86b56ade15793
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-20 13:50:47 +0100

    created a class Artist.php

---
+++ artists_prg/Artist.php
@@ -0,0 +1,13 @@
+<?php
+
+class Artist
+{
+    public $id = null;
+    public $name = null;
+    public $birth = null;
+    public $death = null;
+    public $bio = null;
+    public $country = null;
+    public $movement = null;
+    public $picture_url = null;
+}
---
+++ artists_prg/store.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once "../database/DB_functions.php";
+
