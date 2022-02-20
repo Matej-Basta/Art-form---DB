615990dfca896196e40a098b8a57b6e9c70c4edf
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-20 13:34:41 +0100

    Done form part

---
+++ artists_prg/form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="" method="post">
+
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" value="">
+        <label for="birth">Born:</label>
+        <input type="number" name="birth" id="birth" value="">
+        <label for="death">Died:</label>
+        <input type="number" name="death" id="death" value="">
+        <label for="bio">Bio:</label>
+        <textarea name="bio" id="bio" cols="30" rows="10"></textarea>
+        <label for="country">Nationality:</label>
+        <input type="text" name="country" id="country" value="">
+        <label for="movement">Movement:</label>
+        <input type="text" name="movement" id="movement" value="">
+        <label for="picture_url">URL of picture:</label>
+        <input type="text" name="picture_url" id="picture_url" value="">
+        <button>Save</button>
+    </form>
+
+</body>
+</html>
